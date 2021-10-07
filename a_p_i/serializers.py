323effffe65bc365824dfb4a783005eaa74d918f
TRAMPlin30 <vicodin28@gmail.com>
2021-10-08 00:19:02 +0300

    mixins views -  (vid_18 - vid_21) also APIView

--- a_p_i/serializers.py
+++ a_p_i/serializers.py
@@ -1,13 +1,19 @@
-from rest_framework.serializers import (IntegerField, CharField, Serializer, ModelSerializer)
+from rest_framework.serializers import (IntegerField, CharField, Serializer, ModelSerializer,
+                                        HyperlinkedIdentityField)
 from my_notes.models import MyNotes
 
 
 class NoteSerializer(ModelSerializer):
     class Meta:
         model = MyNotes
-        fields = '__all__'
+        fields = '__all__' # все поля для отображения "id":, "title":, "text": и все остальные
 
-
+#---------------------------------------настраиваемый сериалайзер------------------------------
+#class ThinNoteSerializer(ModelSerializer):
+    #url = HyperlinkedIdentityField(view_name='notes-detail') - очень ругалась Джанга на этот урл Х.З. почему (выяснить !!!!)
+    #class Meta:
+        #model = MyNotes
+        #fields = ('id', 'title', 'url') # отобразит только выбранные поля
 
 #-------------------------------------------------------------------
 #class NoteSerializer(Serializer):
--- a_p_i/urls.py
+++ a_p_i/urls.py
@@ -2,9 +2,20 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from a_p_i.views import *
 
+
+# ----------------lesson 18--------------------------
 urlpatterns = [
-    path('my_notes_list/', my_notes_list, name='my_notes_list'),
-    path('my_notes_list_detail/<int:pk>', my_notes_list_detail, name='my_notes_list_detail'),
+    path('my_notes_list/', My_Notes_List_Mixin.as_view()),
+    path('my_notes_list_detail/<int:pk>', My_Notes_Detail_Mixin.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 
+#--------------------------------------------------------------------------------------
+
+#------------------------------@api_view(['GET', 'POST'])------------------------------------------------------
+#urlpatterns = [
+    #path('my_notes_list/', my_notes_list, name='my_notes_list'),
+    #path('my_notes_list_detail/<int:pk>', my_notes_list_detail, name='my_notes_list_detail'),
+#]
+#urlpatterns = format_suffix_patterns(urlpatterns)
+#------------------------------@api_view(['GET', 'PUT', 'DELETE'])-----------------------------------------------------
